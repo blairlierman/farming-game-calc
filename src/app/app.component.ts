9749fa0cc0c162b59007e44b27aa978be66ea44f
Blair Lierman <blairl@outlook.com>
2020-01-03 22:26:16 -0800

    Bind up values to controls. Calculate the totals and display new values.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,64 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'FarmingGameNetAssetCalculator';
+  totalAmount = 0;
+  hayAmount = 0;
+  grainAmount = 0;
+  fruitAmount = 0;
+  cowAmount = 0;
+  tractorAmount = 0;
+  harvesterAmount = 0;
+  cashPhrase = "Fill in the Assets to begin counting";
+
+  hayAcresChanged(input: number)
+  {  
+    console.log(input);  
+
+    this.hayAmount = input * 2000;
+    this.calculateTotal();
+
+  }
+
+  grainAcresChanged(input: number)
+  {
+    this.grainAmount = input * 2000;
+    this.calculateTotal();
+  }
+
+  fruitAcresChanged(input: number)
+  {
+    this.fruitAmount = input * 5000;
+    this.calculateTotal();
+  }
+
+  cowsChanged(input: number)
+  {
+    this.cowAmount = input * 500;
+    this.calculateTotal();
+  }
+
+  tractorChanged(checked: boolean)
+  {
+    this.tractorAmount = checked ? 10000: 0;
+  }
+
+  harvesterChanged(checked: boolean)
+  {
+    this.harvesterAmount = checked ? 10000: 0;
+  }
+
+  calculateTotal()
+  {
+    this.totalAmount = this.hayAmount + this.grainAmount 
+      + this.fruitAmount + this.cowAmount
+      + this.harvesterAmount + this.tractorAmount;
+    if(this.totalAmount > 0)
+    {
+      this.cashPhrase = `You have \$${this.totalAmount} in Net Assets!`;
+    }
+    else
+    {
+      this.cashPhrase = "Fill in the Assets to begin counting";
+    }
+  }
 }

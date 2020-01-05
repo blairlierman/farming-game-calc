327f49fce3063bb3fe766b2e0c009d64f0fc8494
Blair Lierman <blairl@outlook.com>
2020-01-04 21:59:16 -0800

    Enable Ng-Bootstrap Add Bootstrap CSS to the project Add Bootstrap container to the HTML

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]

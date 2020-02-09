1a4cec406736606d921240a5fbb2b2c104964cb7
Blair Lierman <blairl@outlook.com>
2020-02-08 22:24:10 -0800

    Wire up Increment and Decrement buttons

--- src/app/input-spinner/input-spinner.component.ts
+++ src/app/input-spinner/input-spinner.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 export class InputSpinnerComponent implements OnInit {
 
   @Input() label: string;
-  @Input() step: number = null;
+  @Input() step = 1;
   @Input() id: string;
   @Output() value = new EventEmitter<number>();
 
@@ -22,6 +22,20 @@ export class InputSpinnerComponent implements OnInit {
   onInputChanged(newValue: number) {
     this.inputValue = newValue;
     this.value.emit(this.inputValue);
+  }
+
+  increment() {
+    if (!this.inputValue) { this.inputValue = 0; }
+    if (!this.step) { this.step = 1; }
+
+    this.onInputChanged(this.inputValue + (+this.step)); // + casts this.step to a number to avoid string concatenation
+  }
+
+  decrement() {
+    if (!this.inputValue) { this.inputValue = 0; }
+    if (!this.step) { this.step = 1; }
+
+    this.onInputChanged(this.inputValue - this.step);
   }
 
 }

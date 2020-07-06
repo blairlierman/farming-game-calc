9b2f18c6642b8edbc7703c12f9c41342ce9b949e
Blair Lierman <blairl@outlook.com>
2020-07-05 21:51:31 -0700

    Test increment/decrement methods in input spinner.

--- src/app/touch-input-spinner/touch-input-spinner.component.spec.ts
+++ src/app/touch-input-spinner/touch-input-spinner.component.spec.ts
@@ -4,8 +4,6 @@ import { TouchInputSpinnerComponent } from './touch-input-spinner.component';
 import { FormsModule } from '@angular/forms';
 
 describe('InputSpinnerComponent', () => {
-  let component: TouchInputSpinnerComponent;
-  let fixture: ComponentFixture<TouchInputSpinnerComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -18,12 +16,63 @@ describe('InputSpinnerComponent', () => {
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(TouchInputSpinnerComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+    const fixture = TestBed.createComponent(TouchInputSpinnerComponent);
+    const component: TouchInputSpinnerComponent = fixture.componentInstance;
+
     expect(component).toBeTruthy();
+  });
+
+  it('should increment by 1 with defaults', () => {
+    const fixture = TestBed.createComponent(TouchInputSpinnerComponent);
+    const component: TouchInputSpinnerComponent = fixture.componentInstance;
+
+    component.increment();
+
+    expect(component.inputValue).toEqual(1);
+  });
+
+  it('should increment by passed value of 10', () => {
+    const fixture = TestBed.createComponent(TouchInputSpinnerComponent);
+    const component: TouchInputSpinnerComponent = fixture.componentInstance;
+
+    component.inputValue = 10;
+    component.step = 10;
+
+    component.increment();
+
+    expect(component.inputValue).toEqual(20);
+  });
+
+  it('should attempt to decrement by 1 with default min of 0', () => {
+    const fixture = TestBed.createComponent(TouchInputSpinnerComponent);
+    const component: TouchInputSpinnerComponent = fixture.componentInstance;
+
+    component.decrement();
+
+    expect(component.inputValue).toEqual(0);
+  });
+
+  it('should attempt to decrement by 1', () => {
+    const fixture = TestBed.createComponent(TouchInputSpinnerComponent);
+    const component: TouchInputSpinnerComponent = fixture.componentInstance;
+    component.inputValue = 10;
+
+    component.decrement();
+
+    expect(component.inputValue).toEqual(9);
+  });
+
+  it('should decrement by passed value of 10', () => {
+    const fixture = TestBed.createComponent(TouchInputSpinnerComponent);
+    const component: TouchInputSpinnerComponent = fixture.componentInstance;
+    component.inputValue = 10;
+    component.step = 10;
+
+    component.decrement();
+
+    expect(component.inputValue).toEqual(0);
   });
 });

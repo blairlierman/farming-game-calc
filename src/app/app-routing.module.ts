e6cec2fa27f31f89994529d83fed6815f4a4cd1b
Blair Lierman <blairl@outlook.com>
2020-07-17 22:16:47 -0700

    Add About Page to Routing. Add Header to About Page.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'about', component: AboutPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

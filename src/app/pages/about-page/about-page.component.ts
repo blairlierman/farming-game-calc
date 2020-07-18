d27b37a32a1eb8d180d9b9be849afdc6fc3f7025
Blair Lierman <blairl@outlook.com>
2020-07-17 23:55:15 -0700

    Update About Page NavBar with Back Button. Use a Left Chevron from Font Awesome Solid Icons.

--- src/app/pages/about-page/about-page.component.ts
+++ src/app/pages/about-page/about-page.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { faChevronLeft, IconDefinition } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-about-page',
   templateUrl: './about-page.component.html',
   styleUrls: ['./about-page.component.scss']
 })
-export class AboutPageComponent implements OnInit {
+export class AboutPageComponent {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  faBackIcon: IconDefinition = faChevronLeft;
 
 }
